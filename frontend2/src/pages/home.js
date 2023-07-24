33d0ca9462ac8a62b0cf5f764c71a749a899c4e3
Zisan Karsatar <zisankarsatar@gmail.com>
2023-07-24 19:37:28 +0200

    home page ready

--- frontend2/src/pages/home.js
+++ frontend2/src/pages/home.js
@@ -1,7 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from 'react';
+
 function HomeComponent(){
+    const [products, setProducts] = useState([]);
+
+    const getAll = async () => {
+        const response = await axios.get("http://localhost:3001/products");
+        setProducts(response.data);
+    }
+
+    useEffect(() => {
+        getAll();
+    }, []);
+
     return (
         <>
-        <h1>Home Component</h1>
+        <div className="container">
+            <div className="row">
+                {
+                    products.map((product, index) => (
+                        <div className="col-md-3 mt-4">
+                            <div key={index} className="card">
+                                <div className="card-header"><h4>{product.name}</h4></div>
+                                <div className="card-body">
+                                    <img className="mx-auto d-block" style={{width: "180px", height: '150px',   left: "auto", right: "auto"}} src={"http://localhost:3001/" + product.imageUrl} />
+                                    <h5 className='text-center mt-1' style={{border:"1px solid #ccc", padding: "10px"}}>Adet: {product.stock}</h5>
+                                    <h5 className='text-center text-danger mt-1' style={{border:"1px solid #ccc", padding: "10px"}}>Fiyat: {product.price}</h5>
+                                    <button className="btn btn-outline-success w-100">Add to Basket</button>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
         </>
     )
 }
